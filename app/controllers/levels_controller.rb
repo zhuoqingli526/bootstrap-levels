6fd2316b8ff43b067213a663802170e27861e328
Ben Purinton <ben.purinton@gmail.com>
2024-11-07 12:36:42 -0800

    Autumn 2024 updates

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -2,4 +2,40 @@ class LevelsController < ApplicationController
   def instructions
     render({ :template => "levels/instructions" })
   end
+
+  def primary_button
+    render({ :template => "levels/00_primary_button" })
+  end
+
+  def other_buttons
+    render({ :template => "levels/01_other_buttons" })
+  end
+
+  def utility_classes
+    render({ :template => "levels/02_utility_classes" })
+  end
+
+  def card
+    render({ :template => "levels/03_card" })
+  end
+
+  def modal
+    render({ :template => "levels/04_modal" })
+  end
+
+  def navbar
+    render({ :template => "levels/05_navbar" })
+  end
+
+  def grid
+    render({ :template => "levels/06_grid" })
+  end
+
+  def dynamic_list_group
+    render({ :template => "levels/07_dynamic_list_group" })
+  end
+
+  def toast
+    render({ :template => "levels/08_toast" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "levels", :action => "instructions" })
 
-  get("/levels/primary_button", { :controller => "levels", :action => "primary_button" })
-  get("/levels/other_buttons", { :controller => "levels", :action => "other_buttons" })
-  get("/levels/utility_classes", { :controller => "levels", :action => "utility_classes" })
-  get("/levels/card", { :controller => "levels", :action => "card" })
-  get("/levels/toast", { :controller => "levels", :action => "toast" })
-  get("/levels/modal", { :controller => "levels", :action => "modal" })
-  get("/levels/navbar", { :controller => "levels", :action => "navbar" })
-  get("/levels/grid", { :controller => "levels", :action => "grid" })
-  get("/levels/dynamic_list_group", { :controller => "levels", :action => "dynamic_list_group" })
+  get("/levels/00_primary_button", { :controller => "levels", :action => "primary_button" })
+  get("/levels/01_other_buttons", { :controller => "levels", :action => "other_buttons" })
+  get("/levels/02_utility_classes", { :controller => "levels", :action => "utility_classes" })
+  get("/levels/03_card", { :controller => "levels", :action => "card" })
+  get("/levels/04_modal", { :controller => "levels", :action => "modal" })
+  get("/levels/05_navbar", { :controller => "levels", :action => "navbar" })
+  get("/levels/06_grid", { :controller => "levels", :action => "grid" })
+  get("/levels/07_dynamic_list_group", { :controller => "levels", :action => "dynamic_list_group" })
+  get("/levels/08_toast", { :controller => "levels", :action => "toast" })
 end
